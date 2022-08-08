e30899f200aa1ae03267e74e8d60e1552415e258
AnreX73 <han220173@gmail.com>
2022-08-08 08:58:00 +0700

    models start

--- grantgarant/agency/models.py
+++ grantgarant/agency/models.py
@@ -97,35 +97,40 @@ class FlatState(models.Model):
 
 # тип стройматериалов
 class ObjectConstruction(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Состояние')
+    title = models.CharField(max_length=100, verbose_name='тип стройматериалов')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Состояние'
-        verbose_name_plural = 'Состояние'
+        verbose_name = 'тип стройматериалов'
+        verbose_name_plural = 'тип стройматериалов'
         ordering = ['id']
 
 
 # объект городской недвижимости
 class InCityObject(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
+    price = models.CharField(max_length=255, verbose_name='Цена')
     is_for_sale = models.BooleanField(default=True, verbose_name='Продажа')
     is_for_rent = models.BooleanField(default=False, verbose_name='Аренда')
+    is_hot = models.BooleanField(default=False, verbose_name='горячий вариант')
     object_type = models.ForeignKey(InCityObjectType, on_delete=models.PROTECT, verbose_name='тип объекта')
-    object_adress = models.CharField(max_length=255, verbose_name='Адрес объекта')
+    object_adress = models.CharField(max_length=255, blank=True, verbose_name='адрес объекта')
     city_region = models.ForeignKey(InCityRegion, on_delete=models.PROTECT, verbose_name='район города')
     metro = models.ForeignKey(MetroStation, on_delete=models.PROTECT, verbose_name='станция метро')
-    metro_distance = models.CharField(max_length=255, verbose_name='Расстояние до метро')
+    metro_distance = models.CharField(max_length=255, blank=True, verbose_name='расстояние до метро')
     rooms = models.ForeignKey(RoomAmount, on_delete=models.PROTECT, verbose_name='количество комнат')
-    rooms_layout = models.CharField(max_length=255, verbose_name='планировка')
-    floor = models.CharField(max_length=25, verbose_name='Этаж')
+    square = models.CharField(max_length=255, blank=True, verbose_name='площадь')
+    kitchen = models.CharField(max_length=255, blank=True, verbose_name='площадь кухни')
+    rooms_layout = models.CharField(max_length=255, blank=True, verbose_name='планировка')
+    floor = models.CharField(max_length=25, blank=True, verbose_name='Этаж')
     bathroom = models.ForeignKey(BathroomType, on_delete=models.PROTECT, verbose_name='санузел')
     elevator = models.ForeignKey(ElevatorType, on_delete=models.PROTECT, verbose_name='лифт')
     state = models.ForeignKey(FlatState, on_delete=models.PROTECT, verbose_name='состояние')
     construction = models.ForeignKey(ObjectConstruction, on_delete=models.PROTECT, verbose_name='тип постройки')
-    year = models.CharField(max_length=25, verbose_name='Год постройки / Сдачи')
+    year = models.CharField(max_length=25, blank=True, verbose_name='Год постройки / Сдачи')
+    content = models.TextField(blank=True, verbose_name='текстовое описание ')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
@@ -142,4 +147,87 @@ class OutCityObjectType(models.Model):
     class Meta:
         verbose_name = 'Тип объекта'
         verbose_name_plural = 'Тип объекта'
+        ordering = ['id']
+
+
+# форма собственности
+class TypeOfOwnership(models.Model):
+    title = models.CharField(max_length=155, verbose_name='форма собственности')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'форму собственности'
+        verbose_name_plural = 'форма собственности'
+        ordering = ['id']
+
+
+# электроснабжение
+class Electricity(models.Model):
+    title = models.CharField(max_length=25, verbose_name='Электроснабжение')
+    note = models.CharField(max_length=255, blank=True, verbose_name='примечание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Электроснабжение'
+        verbose_name_plural = 'Электроснабжение'
+        ordering = ['id']
+
+
+# вода
+class Water(models.Model):
+    title = models.CharField(max_length=25, verbose_name='Вода')
+    note = models.CharField(max_length=255, blank=True, verbose_name='примечание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Вода'
+        verbose_name_plural = 'Вода'
+        ordering = ['id']
+
+
+# газ
+class Gas(models.Model):
+    title = models.CharField(max_length=25, verbose_name='Газ')
+    note = models.CharField(max_length=255, blank=True, verbose_name='примечание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Газ'
+        verbose_name_plural = 'Газ'
+        ordering = ['id']
+
+
+# посадки
+class Landings(models.Model):
+    title = models.CharField(max_length=25, verbose_name='Посадки')
+    note = models.CharField(max_length=255, blank=True, verbose_name='примечание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Посадки'
+        verbose_name_plural = 'Посадки'
+        ordering = ['id']
+
+
+# баня
+class Bath(models.Model):
+    title = models.CharField(max_length=25, verbose_name='Баня')
+    note = models.CharField(max_length=255, blank=True, verbose_name='примечание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Баня'
+        verbose_name_plural = 'Баня'
         ordering = ['id']
