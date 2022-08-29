be68fff89d8364e7953d66be878fcf80cc64ae41
AnreX73 <han220173@gmail.com>
2022-08-29 18:35:13 +0700

    аdd include_tag

--- grantgarant/agency/templatetags/agency_tags.py
+++ grantgarant/agency/templatetags/agency_tags.py
@@ -13,7 +13,7 @@ def show_header():
     catalog_link = 'каталог'
     return {"contacts": contacts,
             "logo": logo,
-            "catalog_link":catalog_link,
+            "catalog_link": catalog_link,
             'phone_icon': phone_icon,
             }
 
@@ -22,12 +22,21 @@ def show_header():
 def show_nav():
     in_city_object_type = InCityObjectType.objects.all()
     out_city_object_type = OutCityObjectType.objects.all()
-    
-    return{
-        'in_city_object_type':in_city_object_type,
-        'out_city_object_type':out_city_object_type 
-           
+
+    return {
+        'in_city_object_type': in_city_object_type,
+        'out_city_object_type': out_city_object_type
+
     }
 
 
-    
+@register.inclusion_tag('agency/items_list.html')
+def show_apa(obj_list_type='city', obj_type='Квартира'):
+    if obj_list_type == 'city':
+        selected_items = InCityObject.objects.filter(object_type__title=obj_type).order_by('-time_create')
+    else:
+        selected_items = OutCityObject.objects.filter(object_type__title=obj_type).order_by('-time_create')
+
+    return {
+        'selected_items': selected_items,
+    }
--- grantgarant/agency/views.py
+++ grantgarant/agency/views.py
@@ -17,11 +17,8 @@ def index(request):
 
 def apartments(request):
     context = {
-        'apartments': InCityObject.objects.filter(object_type__title='Квартира').order_by('-time_create'),
         'apartments_icon': Graphics.objects.get(description='иконка квартир'),
-        'new_apartments': InCityObject.objects.filter(object_type__title='Новостройка').order_by('-time_create'),
         'new_apartments_icon': Graphics.objects.get(description='иконка новостроек'),
-        'mini_apartments': InCityObject.objects.filter(object_type__title='Комната').order_by('-time_create'),
         'mini_apartments_icon': Graphics.objects.get(description='иконка комнаты'),
     }
     return render(request, 'agency/apartments.html', context=context)
@@ -29,11 +26,8 @@ def apartments(request):
 
 def dacha(request):
     context = {
-        'dacha': OutCityObject.objects.filter(object_type__title='Дача').order_by('-time_create'),
         'dacha_icon': Graphics.objects.get(description='иконка загородной недвижимости'),
-        'cottage': OutCityObject.objects.filter(object_type__title='Коттедж').order_by('-time_create'),
         'cottage_icon': Graphics.objects.get(description='иконка коттеджа'),
-        'land': OutCityObject.objects.filter(object_type__title='Земельный участок').order_by('-time_create'),
         'land_icon': Graphics.objects.get(description='иконка земельного участка'),
     }
     return render(request, 'agency/dachas.html', context=context)
@@ -45,10 +39,8 @@ def show_apartment(request, apartment_slug):
     context = {
         'apartment': apartment,
         'gallery': Gallery.objects.filter(galleryLink_id=apartment_id)
-
     }
     return render(request, 'agency/apartment.html', context=context)
-
 
 
 def show_dacha(request, dacha_slug):
